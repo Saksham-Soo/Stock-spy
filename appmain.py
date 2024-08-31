67768666d252379f3f009ca08eb323274e00ce80
Saksham-Soo <sakshamraybole2@gmail.com>
2024-08-31 07:58:42 +0530

    Add files via upload

---
+++ appmain.py
@@ -0,0 +1,94 @@
+import numpy as np 
+import pandas as pd 
+import yfinance as yf
+from keras.models import load_model
+import streamlit as st
+import matplotlib.pyplot as plt
+
+model = load_model('c:\\Users\\ASUS\\StockSpy Model.keras')
+
+st.set_page_config ("StockSpy")
+st.header('StockSpy')
+
+stock = st.text_input('Enter Symbol')
+start = '2012-01-01'
+end = '2024-08-30'
+
+data = yf.download(stock, start ,end)
+
+st.subheader('Stock Data for the past 10+ years')
+st.write(data)
+
+data_train = pd.DataFrame(data.Close[0: int(len(data)*0.80)])
+data_test = pd.DataFrame(data.Close[int(len(data)*0.80): len(data)])
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+pas_100_days = data_train.tail(100)
+data_test = pd.concat([pas_100_days, data_train], ignore_index=True)
+data_test_scale = scaler.fit_transform(data_test)
+
+
+st.subheader('Price vs MA50')
+ma_50_days = data.Close.rolling(50).mean()
+fig1 = plt.figure(figsize=(8,6))
+plt.plot(ma_50_days, 'y')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig1)
+
+st.subheader('Price vs MA50 vs MA_100')
+ma_100_days = data.Close.rolling(100).mean()
+fig2 = plt.figure(figsize=(8,6))
+
+
+
+plt.plot(ma_100_days, 'r')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig2)
+
+st.subheader('Price vs MA50 vs MA_100 vs MA_200')
+ma_200_days = data.Close.rolling(200).mean()
+fig3 = plt.figure(figsize=(8,6))
+plt.plot(ma_100_days, 'r')
+plt.plot(ma_200_days, 'b')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig3)
+
+
+x = []
+y = []
+
+for i in range (100, data_test_scale.shape[0]): # type: ignore
+    x.append(data_test_scale[i-100:i])
+    y.append(data_test_scale[i,0])
+
+x,y = np.array(x), np.array(y)
+
+predict = model.predict(x)
+
+scale = 1/scaler.scale_
+
+predict = predict * scale 
+
+st.subheader('Original Price vs Predicted Price')
+fig4 = plt.figure(figsize=(8,6))
+plt.plot(predict, 'r', label='Original Price')
+plt.plot(y, 'g', label = 'Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.show()
+st.pyplot(fig4)
+
+st.write("For more details contact" '(sakshamraybole2@gmail.com)')
+st.write("This web is designed by BS agency and this web help you guys to get the information of a Stock. YOU CAN CONTACT US FOR MORE DETAILS IN THE GIVEN EMAIL-ID")
+
+
+
+st.subheader('CREDITS:') 
+st.write('Saksham Raybole')
+st.write('Baranidharan')
+
